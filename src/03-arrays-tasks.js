93417d40b8950a7a2320e2ec90c42878c8c428fc
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-12-06 21:35:02 +0200

    array tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -20,8 +20,8 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr[value];
 }
 
 /**
@@ -35,8 +35,14 @@ function findElement(/* arr, value */) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  var arr = [];
+  var k=1;
+  for (var i=0; i<len; i++) {
+    arr[i] = k;
+    k = k + 2;
+  };
+  return arr;
 }
 
 
