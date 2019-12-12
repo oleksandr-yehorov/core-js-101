a2e3c36fe9f50eb6edbe71c1290e2ec6214b0168
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-12-13 00:26:17 +0200

    Revert "numbers tasks"
    This reverts commit d0b2cbb2199ab3b3ff9be451ffc1c976ef2db402.

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 /* *******************************************************************************************
  *                                                                                           *
  * Plese read the following tutorial before implementing tasks:                              *
@@ -20,8 +19,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(width, height) {
-  return width * height;
+function getRectangleArea(/* width, height */) {
+  throw new Error('Not implemented');
 }
 
 
@@ -36,8 +35,8 @@ function getRectangleArea(width, height) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCicleCircumference(radius) {
-  return Math.PI * radius * 2;
+function getCicleCircumference(/* radius */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -52,8 +51,8 @@ function getCicleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(value1, value2) {
-  return value1 / 2 + value2 / 2;
+function getAverage(/* value1, value2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -71,8 +70,8 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -87,8 +86,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(a, b) {
-  return -b / a;
+function getLinearEquationRoot(/* a, b */) {
+  throw new Error('Not implemented');
 }
 
 
@@ -110,9 +109,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(x1, y1, x2, y2) {
-  return Math.acos((x1 * x2 + y1 * y2)
-        / (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2)));
+function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -127,8 +125,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(value) {
-  return value.toString()[value.toString().length - 1];
+function getLastDigit(/* value */) {
+  throw new Error('Not implemented');
 }
 
 
@@ -143,8 +141,8 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(value) {
-  return Number(value);
+function parseNumberFromString(/* value */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -160,8 +158,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(a, b, c) {
-  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+function getParallelipidedDiagonal(/* a, b, c */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -181,8 +179,8 @@ function getParallelipidedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(num, pow) {
-  return Math.round(num / (10 ** pow)) * 10 ** pow;
+function roundToPowerOfTen(/* num, pow */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -202,11 +200,8 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(n) {
-  for (let i = 2; i < n; i += 1) {
-    if (n % i === 0) { return false; }
-  }
-  return true;
+function isPrime(/* n */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -224,8 +219,8 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(value, def) {
-  return Number(value) || def;
+function toNumber(/* value, def */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = {
