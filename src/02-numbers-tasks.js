5aaf65c08fca96da5ddeceaeeeac208bc8092698
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-12-13 00:26:39 +0200

    Revert "numbers tasks"
    This reverts commit bb1bd0029ac913e14ab670122a563406475a69a8.

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -89,8 +89,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(a, b) {
-  return -( b / a );
+function getLinearEquationRoot(/* a, b */) {
+  throw new Error('Not implemented');
 }
 
 
@@ -128,8 +128,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(value) {
-  return value % 10;
+function getLastDigit(/* value */) {
+  throw new Error('Not implemented');
 }
 
 
@@ -144,8 +144,8 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(value) {
-  return Number(value);
+function parseNumberFromString(/* value */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -161,8 +161,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(a, b, c) {
-  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+function getParallelipidedDiagonal(/* a, b, c */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -182,8 +182,8 @@ function getParallelipidedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(num, pow) {
-  return (num / 10 ** pow).toFixed() * 10 ** pow;
+function roundToPowerOfTen(/* num, pow */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -203,12 +203,8 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(n) {
-  for (let i = 2, nSqrt = Math.sqrt(n); i <= nSqrt; i += 1) {
-    if (n % i === 0) return false;
-  }
-
-  return n > 1;
+function isPrime(/* n */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -226,8 +222,8 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(value, def) {
-  return Number(value) || def;
+function toNumber(/* value, def */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = {
